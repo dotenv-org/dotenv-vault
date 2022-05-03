124f8d1f50ab8158fd1644af372e4b55559313bb
Rafael Corrêa Chaves <rafael.chaves98@hotmail.com>
2022-05-03 14:29:09 -0300

    chore: updated test references

--- test/services/append-to-ignore-service.test.ts
+++ test/services/append-to-ignore-service.test.ts
@@ -2,7 +2,7 @@ import * as fs from 'node:fs'
 import * as sinon from 'sinon'
 import {expect} from '@oclif/test'
 
-import {AppendToGitignoreService} from '../../src/services/append-to-gitignore-service'
+import {AppendToIgnoreService} from '../../src/services/append-to-ignore-service'
 
 describe('run', async () => {
   beforeEach( () => {
