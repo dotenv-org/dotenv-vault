4a7be7524e285f7ff2e3ab900871761070312c3b
Scott Motte <mot@mot.la>
2022-06-18 08:13:43 -0700

    Add whoami

---
+++ src/commands/whoami.ts
@@ -0,0 +1,29 @@
+import {Command, Flags} from '@oclif/core'
+
+import {WhoamiService} from '../services/whoami-service'
+
+export default class Whoami extends Command {
+  static description = 'Display the current logged in user'
+
+  static examples = [
+    '<%= config.bin %> <%= command.id %>',
+  ]
+
+  static flags = {
+    dotenvMe: Flags.string({
+      char: 'm',
+      description: 'Pass .env.me (DOTENV_ME) credential directly (rather than reading from .env.me file)',
+      hidden: false,
+      multiple: false,
+      env: 'DOTENV_ME',
+      required: false,
+    }),
+  }
+
+  public async run(): Promise<void> {
+    const {flags} = await this.parse(Whoami)
+    const dotenvMe = flags.dotenvMe
+
+    new WhoamiService({cmd: this, dotenvMe: dotenvMe}).run()
+  }
+}
