3f3c634225ec23f5c5b9d3870c531dd202294a8c
Scott Motte <mot@mot.la>
2023-01-27 14:55:07 -0800

    Add decrypt

--- src/services/decrypt-service.ts
+++ src/services/decrypt-service.ts
@@ -1,9 +1,16 @@
-import {readFileSync} from 'fs'
 import {LogService} from '../services/log-service'
+
+import {config} from 'dotenv'
+import {decrypt} from 'dotenv-vault-core'
 
 interface DecryptServiceAttrs {
   cmd;
   dotenvKey;
+}
+
+type InstructionsType = {
+  ciphertext: string;
+  key: string;
 }
 
 class DecryptService {
@@ -19,15 +26,63 @@ class DecryptService {
   }
 
   async run(): Promise<void> {
-    this.log.plain(this.dotenvVaultContents)
+    const result = config({path: this.vaultPath})
+    const keys = this.dotenvKey.split(',')
+    const length = keys.length
+
+    let decrypted
+    for (let i = 0; i < length; i++) {
+      try {
+        // Get full key
+        const key = keys[i].trim()
+
+        // Get instructions for decrypt
+        const attrs = this._instructions(result, key)
+
+        // Decrypt
+        decrypted = decrypt(attrs.ciphertext, attrs.key)
+
+        break
+      } catch (error) {
+        // last key
+        if (i + 1 >= length) {
+          throw error
+        }
+        // try next key
+      }
+    }
+
+    this.log.plain(decrypted)
   }
 
-  get dotenvVault(): string {
+  _instructions(result: string, dotenvKey: string): InstructionsType {
+    // Parse DOTENV_KEY. Format is a URI
+    const uri = new URL(dotenvKey)
+
+    // Get decrypt key
+    const key = uri.password
+    if (!key) {
+      throw new Error('INVALID_DOTENV_KEY: Missing key part')
+    }
+
+    // Get environment
+    const environment = uri.searchParams.get('environment')
+    if (!environment) {
+      throw new Error('INVALID_DOTENV_KEY: Missing environment part')
+    }
+
+    // Get ciphertext payload
+    const environmentKey = `DOTENV_VAULT_${environment.toUpperCase()}`
+    const ciphertext = result.parsed[environmentKey] // DOTENV_VAULT_PRODUCTION
+    if (!ciphertext) {
+      throw new Error(`NOT_FOUND_DOTENV_ENVIRONMENT: Cannot locate environment ${environmentKey} in your .env.vault file. Run 'npx dotenv-vault build' to include it.`)
+    }
+
+    return {ciphertext, key}
+  }
+
+  get vaultPath(): string {
     return '.env.vault'
-  }
-
-  get dotenvVaultContents(): string {
-    return readFileSync(this.dotenvVault, 'utf8')
   }
 }
 
